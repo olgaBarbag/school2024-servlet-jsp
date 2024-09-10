5d60c67ae470118c82df3cb589a72aab6c2476b1
Olga B. <olbarbag@gmail.com>
2024-09-10 19:36:51 +0300

    FiltersDTO data from the filters client provides

---
+++ src/main/java/gr/aueb/cf/school2024app/dto/FiltersDTO.java
@@ -0,0 +1,11 @@
+package gr.aueb.cf.school2024app.dto;
+
+public class FiltersDTO extends BaseDTO {
+
+    public FiltersDTO() {
+    }
+
+    public FiltersDTO(String firstName, String lastName) {
+        super(firstName, lastName);
+    }
+}
